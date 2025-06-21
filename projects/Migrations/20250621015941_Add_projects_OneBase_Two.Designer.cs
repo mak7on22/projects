406537e6e2103579b175353adc0af0886db7a527
maksim <maksim@DESKTOP-9D1MEP9>
2025-06-21 05:00:22 +0300

    added new migrations

--- projects/Migrations/20250621015941_Add_projects_OneBase_Two.Designer.cs
+++ projects/Migrations/20250621015941_Add_projects_OneBase_Two.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using projects.Models;
@@ -11,9 +12,11 @@ using projects.Models;
 namespace projects.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250621015941_Add_projects_OneBase_Two")]
+    partial class Add_projects_OneBase_Two
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
